6deff6279d0dc586e2d304106fde4128fe4fa5b2
surya <s2g.bestprogrammer@gmail.com>
2022-03-23 19:37:07 +0800

    main.blade

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,10 +10,10 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="../node_modules/jqvmap/dist/jqvmap.min.css">
-  <link rel="stylesheet" href="../node_modules/weathericons/css/weather-icons.min.css">
-  <link rel="stylesheet" href="../node_modules/weathericons/css/weather-icons-wind.min.css">
-  <link rel="stylesheet" href="../node_modules/summernote/dist/summernote-bs4.css">
+  <link rel="stylesheet" href="/node_modules/jqvmap/dist/jqvmap.min.css">
+  <link rel="stylesheet" href="/node_modules/weathericons/css/weather-icons.min.css">
+  <link rel="stylesheet" href="/node_modules/weathericons/css/weather-icons-wind.min.css">
+  <link rel="stylesheet" href="/node_modules/summernote/dist/summernote-bs4.css">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
